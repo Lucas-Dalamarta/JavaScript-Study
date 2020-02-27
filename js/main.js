1e146a04b7d015eef7a848ea4e2fe36afd9cbbcd
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2020-02-26 22:37:43 -0300

    Startando o basico do JS

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var name = "Luke Skywalker";
+var idade = 23;
+var idade2 = 10;
+var frase = "Naruto é melhor que Boruto"
+
+console.log(idade+idade2);
+console.log(frase.replace("Naruto","Dragon Ball"));
